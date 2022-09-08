616a77ea7c111ff7fbadb028e2bb06811da8420c
Inès Bourhim <ines.bourhim@mission-rgpd.com>
2022-09-08 14:49:24 +0200

    Tableau Html

---
+++ src/main/java/com/example/springtuto2/DataInitialize.java
@@ -0,0 +1,4 @@
+package com.example.springtuto2;
+
+public class DataInitialize {
+}
--- src/main/java/com/example/springtuto2/controller/BaseLegaleController.java
+++ src/main/java/com/example/springtuto2/controller/BaseLegaleController.java
@@ -1,19 +1,27 @@
 package com.example.springtuto2.controller;
 
 import com.example.springtuto2.model.BaseLegale;
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.*;
 
-@Controller
+@RestController
 public class BaseLegaleController {
 
-
+    @Autowired
+    JdbcTemplate jdbcTemplate;
     private ArrayList<BaseLegale> base = new ArrayList<>();
 
+    @GetMapping("/base")
+    public List<BaseLegale> listALl(){
+        String sql ="SELECT * FROM baseLegale";
+        return jdbcTemplate.query(sql, BeanPropertyRowMapper.newInstance(BaseLegale.class));
+    }
     @GetMapping("/msg")
     public String showBase(Model model){
         BaseLegale b1 = new BaseLegale("0111l00","Inès","150sdsqd");
